b0f941d83b1f1c1c00d82aa90171538491a3e141
am009 <634960348@qq.com>
2019-12-07 09:09:29 +0800

    :sparkles: a unfinished logger, and a thread pool

--- ComputerNetworkExperiment/MessageQueue.h
+++ ComputerNetworkExperiment/MessageQueue.h
@@ -1,5 +1,138 @@
 #pragma once
+
+#include <mutex>
+#include <queue>
+#include <functional>
+#include <future>
+#include <thread>
+#include <utility>
+#include <vector>
+
+namespace MQ {
+	template <typename T>
+	class Queue {
+	private:
+		std::queue<T> queue;
+		std::mutex m;
+	public:
+		Queue() {}
+		
+		~Queue() {}
+		
+		bool empty()
+		{
+			std::unique_lock<std::mutex> lock(m);
+			return queue.empty();
+		}
+
+		int size()
+		{
+			std::unique_lock<std::mutex> lock(m);
+			return queue.size();
+		}
+		
+		void enqueue(T& t)
+		{
+			std::unique_lock<std::mutex> lock(m);
+			queue.push(t);
+		}
+
+		bool dequeue(T& t)
+		{
+			std::unique_lock<std::mutex> lock(m);
+			if (queue.empty())
+			{
+				return false;
+			}
+			t = std::move(queue.front());
+			queue.pop();
+			return true;
+		}
+
+	};
+}
+
 class MessageQueue
 {
+private:
+	class Worker
+	{
+	private:
+		int id;
+		MessageQueue* mq;
+	public:
+		Worker(MessageQueue* mq, const int id) : mq(mq), id(id) {}
+		void operator()()
+		{
+			std::function<void()> func;
+			bool dequeued;
+			while (!mq->shutdown)
+			{
+				std::unique_lock<std::mutex> lock(mq->condititonal_mutex);
+				if (mq->queue.empty())
+				{
+					mq->cv.wait(lock);
+				}
+				dequeued = mq->queue.dequeue(func); // get task
+				if (dequeued)
+				{
+					func();
+				}
+			}
+		}
+	};
+
+	bool shutdown;
+	MQ::Queue<std::function<void()>> queue;
+	std::vector<std::thread> threads;
+	std::mutex condititonal_mutex;
+	std::condition_variable cv;
+public:
+	MessageQueue(const int n) : threads(std::vector<std::thread>(n)), shutdown(false) {}
+	
+	MessageQueue(const MessageQueue&) = delete; // not allowed to use
+	MessageQueue(MessageQueue&&) = delete;
+
+	MessageQueue& operator=(const MessageQueue&) = delete;
+	MessageQueue& operator=(MessageQueue&&) = delete;
+
+	void init()
+	{
+		int size = threads.size();
+		for (int i = 0; i < size; ++i)
+		{
+			threads[i] = std::thread(Worker(this, i));
+		}
+	}
+
+	// Waits until threads finish their current task and shutdowns the pool
+	void shutdown()
+	{
+		shutdown = true;
+		cv.notify_all();
+
+		int size = threads.size();
+		for (int i = 0; i < size; ++i)
+		{
+			if (threads[i].joinable())
+			{
+				threads[i].join();
+			}
+		}
+	}
+
+	template<typename F, typename...Args>
+	auto submitFunc(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+		std::function<decltype(f(args...))()> func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+		auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+		std::function<void()> wrapper_func = [task_ptr]()
+		{
+			(*task_ptr)();
+		};
+
+		queue.enqueue(wrapper_func);
+		cv.notify_one();
+		return task_ptr->get_future();
+	}
 };
 
--- ComputerNetworkExperiment/logger.h
+++ ComputerNetworkExperiment/logger.h
@@ -1,5 +1,34 @@
 #pragma once
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <tchar.h>
+#include <ctime>
+#include <mutex>
+
 class logger
 {
+public:
+	
+
+	static void info(std::string x) { self->log(std::string("[INFO]") + x); }
+	static void warning(std::string x) { self->log(std::string("[WARNING]") + x); }
+	static void critical(std::string x) { self->log(std::string("[CRITICAL]") + x); }
+	static void debug(std::string x) { self->log(std::string("[DEBUG]") + x); }
+	static void error(std::string x) { self->log(std::string("[ERROR]") + x); }
+
+private:
+	logger(const std::string LogFile);
+	~logger();
+
+	void log(std::string x);
+	static std::string GetFilePath();
+	static std::string path;
+	std::ofstream ofs;
+	static logger* self;
+	static std::mutex create;
+	static std::string get_time();
+	static bool is_path_exist(const std::string FilePath);
 };
+
 
