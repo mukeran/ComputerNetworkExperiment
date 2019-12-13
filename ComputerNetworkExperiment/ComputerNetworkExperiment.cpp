5f89b913b37d1b4ec05fab255f991d9f3ef14808
am009 <634960348@qq.com>
2019-12-13 21:36:07 +0800

    messageQueue

--- ComputerNetworkExperiment/ComputerNetworkExperiment.cpp
+++ ComputerNetworkExperiment/ComputerNetworkExperiment.cpp
@@ -1,7 +1,30 @@
-﻿#include "http/server.h"
+﻿//#include "http/server.h"
+//
+//int main()
+//{
+//	http::server server("0.0.0.0", 10010);
+//	server.start();
+//}
+
+
+#include "SMTPClient.h"
+#include "MessageQueue.h"
+
+
+void simulate_hard_computation() {
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+}
+
+void multiply(const int a, const int b) {
+	simulate_hard_computation();
+	const int res = a * b;
+	std::cout << a << " * " << b << " = " << res << std::endl;
+}
 
 int main()
 {
-	http::server server("0.0.0.0", 10010);
-	server.start();
+	MessageQueue queue(2);
+	queue.init();
+	queue.submitFunc(multiply, 3, 5);
+	return 0;
 }
